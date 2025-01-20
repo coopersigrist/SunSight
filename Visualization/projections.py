f39fd5309074eec47a2eb7faecd1f4898109a342
Mimi Lertsaroj <mimi.lertsaroj.3@gmail.com>
2025-01-19 23:24:33 -0500

    new changes to projections.py

--- Visualization/projections.py
+++ Visualization/projections.py
@@ -10,7 +10,7 @@ print(combined_df)
 objectives = ['carbon_offset_kg_per_panel','energy_generation_per_panel','black_prop','Median_income','yearly_sunlight_kwh_kw_threshold_avg','carbon_offset_metric_tons_per_panel']
 n = normalise_df(combined_df, objectives)
 
-max_num_added = 10000
+max_num_added = 100000
 Energy_projections, Energy_picked = create_projections(combined_df, n=max_num_added, load=False, metric='energy_generation_per_panel')
 Carbon_offset_projections, Carbon_offset_picked = create_projections(combined_df, n=max_num_added, load=False, metric='carbon_offset_kg_per_panel')
 Income_equity_projections, Income_equity_picked = create_projections(combined_df, n=max_num_added, load=False, metric='Median_income')
@@ -118,6 +118,38 @@ def df_with_updated_picks(combined_df, picks, load=None, save=None):
     #     new_df.to_csv(save, index=False)
 
     return new_df
+
+#sped up version 
+# def df_with_updated_picks(combined_df, picks, load=None, save=None):
+#     # if load is not None and exists(load):
+#     #     return pd.read_csv(load)
+
+#     new_df = combined_df.copy()
+#     new_co = new_df['carbon_offset_metric_tons'].to_numpy()
+#     new_existing = new_df['existing_installs_count'].to_numpy()
+
+#     # Create a mapping from region names to indices for faster access
+#     # region_index_map = {region: index for index, region in enumerate(new_df['region_name'])}
+#     # Update carbon offset and existing installs using vectorized operations
+#     for pick in tqdm(picks):
+#         # if pick in region_index_map:  # Check if the region exists in the map
+#         index = list(new_df['region_name']).index(pick)
+#         new_co[index] -= new_df['carbon_offset_metric_tons_per_panel'].iloc[index]
+#         new_existing[index] += 1
+#         # else:
+#         #     print("no")
+
+#     print('Number install change:', np.sum(new_existing - new_df['existing_installs_count']))
+    
+#     # Update DataFrame columns with new values
+#     new_df['existing_installs_count'] = new_existing
+#     new_df['existing_installs_count_per_capita'] = new_existing / new_df['Total_Population']
+#     new_df['panel_utilization'] = new_existing / new_df['number_of_panels_total']
+
+#     # if save is not None:
+#     #     new_df.to_csv(save)
+    
+#     return new_df
 
 def plot_demo_state_stats(new_df,save="/Users/mimilertsaroj/Desktop/SunSight/Visualization/Clean_Data/data_by_state_proj.csv"):
     # state_df = load_state_data(new_df, load=None, save=save)
@@ -249,7 +281,7 @@ def grid_search(combined_df, npanels, metrics, objectives, save=None, load=None)
     """Perform grid search to find Pareto-optimal solutions."""
     weight_starts = [0.0, 0.0, 0.0, 0.0]
     weight_ends = [2.0, 2.0, 2.0, 2.0]
-    number_of_samples = 12
+    number_of_samples = 6
     filename = "/Users/mimilertsaroj/Desktop/SunSight/Visualization/Projection_Data/GS" + str(number_of_samples)
     
     # Precompute grid of weights
@@ -267,15 +299,25 @@ def grid_search(combined_df, npanels, metrics, objectives, save=None, load=None)
         local_results = {}
         for weights in weight_grid:
             weights_tuple = tuple(weights)
+            if weights[0]==weights[1]:
+                continue
+            if weights[0]==weights[1] and weights[1]==weights[2] and weights[2]==weights[3] and weights[0]!=1:
+                continue
             projection, picked = create_weighted_proj(combined_df, n=npanels, objectives=objectives, weights=weights, metric=metric)
             if metric in ['black_prop', 'Median_income']:
                 new_picks_df = df_with_updated_picks(combined_df, picked)
                 key = "panel_utilization"
                 demo = 'black_prop' if metric == 'black_prop' else 'Median_income'
                 median = np.median(new_picks_df[demo].values)
+                # print(new_picks_df[new_picks_df[demo] < median][key].values)
                 low_avg = np.mean(new_picks_df[new_picks_df[demo] < median][key].values)
                 high_avg = np.mean(new_picks_df[new_picks_df[demo] >= median][key].values)
-                equity_score = np.abs(1 - np.abs(high_avg - low_avg))
+                true_avg = np.mean(new_picks_df[key].values)
+                #1 - ((1 - (1 - H-L)) / avg) = 1 - (H-L)/avg
+                equity_score = np.abs(1 - (np.abs(high_avg - low_avg)/true_avg))
+                print(true_avg)
+                print("wah")
+                print(equity_score)
                 local_results[weights_tuple] = equity_score
             elif metric == 'carbon_offset_kg_per_panel':
                 local_results[weights_tuple] = projection[-1] / co_baseline
@@ -284,8 +326,8 @@ def grid_search(combined_df, npanels, metrics, objectives, save=None, load=None)
         return metric, local_results
 
     # Parallelise metric computation
-    metrics_results = Parallel(n_jobs=-1)(delayed(process_metric)(metric) for metric in metrics)
-
+    # metrics_results = Parallel(n_jobs=-1)(delayed(process_metric)(metric) for metric in metrics)
+    metrics_results = process_metric("black_prop")
     # Combine results
     for metric, metric_results in metrics_results:
         for weights_tuple, value in metric_results.items():
@@ -298,15 +340,17 @@ def grid_search(combined_df, npanels, metrics, objectives, save=None, load=None)
     df.to_csv(filename, index=False)
 
     return df
-def pareto_calc(df):  
+def pareto_calc(df= pd.read_csv("/Users/mimilertsaroj/Desktop/SunSight/Visualization/Projection_Data/GS7")):  
     pareto_opt_solutions = []
-    filename = "/Users/mimilertsaroj/Desktop/SunSight/Visualization/Projection_Data/GSParetoOptSoln12"
+    filename = "/Users/mimilertsaroj/Desktop/SunSight/Visualization/Projection_Data/GSParetoOptSoln7"
     for index, row in df.iterrows():
         add = True
         for i, optsoln in enumerate(pareto_opt_solutions):
+            # print(row[4:])
+            # print(optsoln[4:])
             if dominates(row[4:],optsoln[4:]):
                 del(pareto_opt_solutions[i])
-            if dominates(optsoln, row): 
+            elif dominates(optsoln, row): 
                 add = False
                 break
         if add == True:
@@ -317,18 +361,37 @@ def pareto_calc(df):
     new_df.to_csv(filename, index=False)
     return df
 
+def plot_pareto(pareto_df, solution_df, rr_co, rr_eng, x_metric, y_metric,x_rr, y_rr):
+    """Plotting pareto front"""
+    plt.figure(figsize=(10, 6))
+
+    plt.scatter(solution_df[x_metric], solution_df[y_metric], label='All Points', alpha=0.5)
+    plt.scatter(pareto_df[x_metric], pareto_df[y_metric], color='red', label='Pareto Optimal', s=80)
+    print("wah")
+    print(rr_co)
+    plt.scatter(rr_co.values[-1]/Carbon_offset_projections['Status-Quo'].values[-1], rr_eng.values[-1]/Energy_projections['Status-Quo'].values[-1], color='green', marker='x', label='Round Robin Results', s=100)
+
+    plt.xlabel(x_metric)
+    plt.ylabel(y_metric)
+    plt.title('Pareto Front Visualization')
+    plt.legend()
+    plt.grid(alpha=0.3)
+    plt.show()
 
 
-metrics = ['carbon_offset_kg_per_panel', 'energy_generation_per_panel', 'black_prop','Median_income']
-objectives = ['carbon_offset_kg_per_panel', 'energy_generation_per_panel', 'black_prop','Median_income']
-df = grid_search(combined_df, npanels=10000, metrics = metrics, objectives = objectives)
-print(df)
-pareto_set = pareto_calc(df)
+# metrics = ['carbon_offset_kg_per_panel', 'energy_generation_per_panel', 'black_prop','Median_income']
+# objectives = ['carbon_offset_kg_per_panel', 'energy_generation_per_panel', 'black_prop','Median_income']
+# df = grid_search(combined_df, npanels=max_num_added, metrics = metrics, objectives = objectives)
+# print(df)
+pareto_set = pareto_calc()
 print(pareto_set)
-# print(pd.read_csv("/Users/mimilertsaroj/Desktop/SunSight/Visualization/Projection_Data/GSParetoOptSoln5"))
 
-plot_projections(Carbon_offset_projections, interval=100, upper_bound='Carbon-Efficient', ylabel="Carbon Offset (kg)")
-plot_projections(Energy_projections, interval=100, upper_bound='Energy-Gen', ylabel="Energy Gen (kW/h)")
+# pareto_df = pd.read_csv("/Users/mimilertsaroj/Desktop/SunSight/Visualization/Projection_Data/GSParetoOptSoln5")
+# solution_df = pd.read_csv("/Users/mimilertsaroj/Desktop/SunSight/Visualization/Projection_Data/GS5")
+# plot_pareto(pareto_df, solution_df, rr_co = Carbon_offset_projections['Round Robin'], rr_eng = Energy_projections['Round Robin'], x_metric= "CO (prop to SQ)", y_metric="Energy gen (prop to SQ)", x_rr ="carbon_offset_kg_per_panel", y_rr =  "energy_generation_per_panel")
+# print("done")
+# plot_projections(Carbon_offset_projections, interval=100, upper_bound='Carbon-Efficient', ylabel="Carbon Offset (kg)")
+# plot_projections(Energy_projections, interval=100, upper_bound='Energy-Gen', ylabel="Energy Gen (kW/h)")
 
 # coprojections, copicked = create_weighted_proj(combined_df,1000, objectives = objectives,weights = soln, metric = 'carbon_offset_kg_per_panel')
 # engprojections, engpicked = create_weighted_proj(combined_df,1000, objectives = objectives,weights = soln, metric = 'energy_generation_per_panel')
@@ -342,7 +405,7 @@ plot_projections(Energy_projections, interval=100, upper_bound='Energy-Gen', yla
 # blackprojections, blackpicked = create_weighted_proj(combined_df,1000, objectives = objectives,weights = soln, metric = 'black_prop')
 # incomeprojections, incomepicked = create_weighted_proj(combined_df,1000, objectives = objectives,weights = soln, metric = 'Median_income')
 
-blackweighted_df = df_with_updated_picks(combined_df, Racial_equity_picked['Weighted Greedy'], load='Projection_Data/df_greedy_weighted_gs.csv', save='/Users/mimilertsaroj/Desktop/SunSight/Visualization/Projection_Data/df_greedy_weighted_gs.csv')
+# blackweighted_df = df_with_updated_picks(combined_df, Racial_equity_picked['Weighted Greedy'], load='Projection_Data/df_greedy_weighted_gs.csv', save='/Users/mimilertsaroj/Desktop/SunSight/Visualization/Projection_Data/df_greedy_weighted_gs.csv')
 
 # # # plot_demo_state_stats(round_robin_df, save="Projection_Data/data_by_state_proj_greedy_round_robink.csv")
 # plot_demo_state_stats(blackweighted_df, save=None)
@@ -350,7 +413,7 @@ blackweighted_df = df_with_updated_picks(combined_df, Racial_equity_picked['Weig
 # incomeweighted_df = df_with_updated_picks(combined_df, Income_equity_picked['Weighted Greedy'], load='Projection_Data/df_greedy_weighted_gs.csv', save='/Users/mimilertsaroj/Desktop/SunSight/Visualization/Projection_Data/df_greedy_weighted_gs.csv')
 
 # # plot_demo_state_stats(round_robin_df, save="Projection_Data/data_by_state_proj_greedy_round_robink.csv")
-plot_demo_state_stats(blackweighted_df, save=None)
+# plot_demo_state_stats(blackweighted_df, save=None)
 
 
 
