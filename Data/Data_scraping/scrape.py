d0d4be7dfa62a8b26aecdb0896a0a599bf882e32
Anu <anusitaraman@gmail.com>
2025-07-16 21:26:50 -0400

    mod to scrape util (adding region mappings, adding zip to region), adding more to scrape

--- Data/Data_scraping/scrape.py
+++ Data/Data_scraping/scrape.py
@@ -1,4 +1,4 @@
-from scrape_util import *
+from Data.Data_scraping.scrape_util import *
 
 # Define which codes to use and what they are here, find the full instructions for these codes here:
 # https://www.census.gov/programs-surveys/acs/technical-documentation/code-lists.html
@@ -13,8 +13,39 @@ code_dict = {'B01003_001E': 'Total_Population',
                     'B02001_004E': 'native_population',
                     'B03003_003E' : 'hispanic_population'}
 
+code_dict_new = {
+    'B19001_002E':'Income: < $10,000',
+    'B19001_003E':'Income: $10,000-$14,999',
+    'B19001_004E':'Income: $15,000-$19,999',
+    'B19001_005E': 'Income: $20,000-$24,999',
+    'B19001_006E': 'Income: $25,000-$29,999',
+    'B19001_007E': 'Income: $30,000-$34,999',
+    'B19001_008E': 'Income: $35,000-$39,999',
+    'B19001_009E': 'Income: $40,000-$44,999',
+    'B19001_010E': 'Income: $45,000-$49,999',
+    'B19001_011E': 'Income: $50,000-$59,999',
+    'B19001_012E': 'Income: $60,000-$74,999',
+    'B19001_013E': 'Income: $75,000-$99,999',
+    'B19001_014E':'Income: $100,000-$124,999',
+    'B19001_015E': 'Income: $125,000-$149,999',
+    'B19001_016E':'Income: $150,000-$199,999',
+    'B19001_017E':'Income: $200,000+',
+
+
+    'B11001_001E': 'total_households',
+    'B19013_001E': 'Median_income',
+    'B19301_001E': 'per_capita_income',
+    'B17001_002E': 'households_below_poverty_line', 
+    'B02001_003E': 'black_population',
+    'B02001_002E': 'white_population',  
+    'B02001_005E' : 'asian_population', 
+    'B02001_004E': 'native_population',
+    'B03003_003E' : 'hispanic_population'
+}
+
 # Downloads and save census data by ZIP code -- downloads features listed in the code dict above
-census_df = get_census_info_by_zip_codes(save_dir="../Census/census_by_zip_w_hisp.csv",code_dict=code_dict)
+#census_df = get_census_info_by_zip_codes(save_dir="../Census/census_by_zip_w_hisp.csv",code_dict=code_dict)
+census_df = get_census_info_by_zip_codes(save_dir="census_by_zip_complex.csv",code_dict=code_dict_new)
 
 # Downloads and saves the project sunroof data at zip, city, and state granularity
 # project_sunroof_scrape()
--- Data/Data_scraping/scrape_util.py
+++ Data/Data_scraping/scrape_util.py
@@ -6,7 +6,7 @@ import pandas as pd
 import csv
 
 # from geopy.geocoders import Nominatim # For address to lat/long
-# from uszipcode import SearchEngine
+from uszipcode import SearchEngine
 from tqdm import tqdm
 
 from os.path import exists
@@ -14,6 +14,76 @@ from os.path import exists
 from urllib.request import urlopen
 import urllib
 import ast
+state_to_region_division = {
+    # Northeast
+    'CT': ('Northeast', 'New England'),
+    'ME': ('Northeast', 'New England'),
+    'MA': ('Northeast', 'New England'),
+    'NH': ('Northeast', 'New England'),
+    'RI': ('Northeast', 'New England'),
+    'VT': ('Northeast', 'New England'),
+    'NJ': ('Northeast', 'Middle Atlantic'),
+    'NY': ('Northeast', 'Middle Atlantic'),
+    'PA': ('Northeast', 'Middle Atlantic'),
+
+    # Midwest
+    'IN': ('Midwest', 'East North Central'),
+    'IL': ('Midwest', 'East North Central'),
+    'MI': ('Midwest', 'East North Central'),
+    'OH': ('Midwest', 'East North Central'),
+    'WI': ('Midwest', 'East North Central'),
+    'IA': ('Midwest', 'West North Central'),
+    'KS': ('Midwest', 'West North Central'),
+    'MN': ('Midwest', 'West North Central'),
+    'MO': ('Midwest', 'West North Central'),
+    'NE': ('Midwest', 'West North Central'),
+    'ND': ('Midwest', 'West North Central'),
+    'SD': ('Midwest', 'West North Central'),
+
+    # South
+    'DE': ('South', 'South Atlantic'),
+    'FL': ('South', 'South Atlantic'),
+    'GA': ('South', 'South Atlantic'),
+    'MD': ('South', 'South Atlantic'),
+    'NC': ('South', 'South Atlantic'),
+    'SC': ('South', 'South Atlantic'),
+    'VA': ('South', 'South Atlantic'),
+    'DC': ('South', 'South Atlantic'),
+    'WV': ('South', 'South Atlantic'),
+    'AL': ('South', 'East South Central'),
+    'KY': ('South', 'East South Central'),
+    'MS': ('South', 'East South Central'),
+    'TN': ('South', 'East South Central'),
+    'AR': ('South', 'West South Central'),
+    'LA': ('South', 'West South Central'),
+    'OK': ('South', 'West South Central'),
+    'TX': ('South', 'West South Central'),
+
+    # West
+    'AZ': ('West', 'Mountain'),
+    'CO': ('West', 'Mountain'),
+    'ID': ('West', 'Mountain'),
+    'MT': ('West', 'Mountain'),
+    'NV': ('West', 'Mountain'),
+    'NM': ('West', 'Mountain'),
+    'UT': ('West', 'Mountain'),
+    'WY': ('West', 'Mountain'),
+    'AK': ('West', 'Pacific'),
+    'CA': ('West', 'Pacific'),
+    'HI': ('West', 'Pacific'),
+    'OR': ('West', 'Pacific'),
+    'WA': ('West', 'Pacific')
+}
+#connecting the zipcode to region/division
+def zip_to_region_division(zipcode, search_engine):
+    zipcode_info = search_engine.by_zipcode(zipcode)
+    state_abbr = zipcode_info.state_abbr
+
+    if not state_abbr:
+        return None, None, None  
+
+    region, division = state_to_region_division.get(state_abbr, (None, None))
+    return state_abbr, region, division
 
 # Downloads csv's of the project sunroof data at different granularities and saves them
 def project_sunroof_scrape():
@@ -50,7 +120,15 @@ def get_census_info_by_zip_codes(code_dict, save_dir="../Census/census_by_zip.cs
     df = pd.DataFrame(res[1:],columns=res[0])
     df = df.rename(columns=code_dict)
     df = df.rename(columns={'zip code tabulation area':'zcta'})
+
+    #adds state_abbr, region, and division to the df
+    search_engine = SearchEngine()
     
+
+    df['state_abbr'] = df['zcta'].apply(lambda x: zip_to_region_division(x, search_engine)[0])
+    df['region'] = df['zcta'].apply(lambda x: zip_to_region_division(x, search_engine)[1])
+    df['division'] = df['zcta'].apply(lambda x: zip_to_region_division(x, search_engine)[2])
+
     # Saves the Census data 
     df.to_csv(save_dir, index=False)
 
